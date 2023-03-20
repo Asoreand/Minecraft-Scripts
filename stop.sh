bff0933b47d667b82823590c66ab1a8fb58ac6dd
Edgar <25728519+Asoreand@users.noreply.github.com>
2023-03-20 20:56:34 +0100

    Added timer in terminal
    Added timer in terminal and changed "shutting down" to "stopping"

--- stop.sh
+++ stop.sh
@@ -6,16 +6,22 @@ stop() {
     # Screen window in which the server is running
     screen_session=$1
 
-    # Warn online players that it's closing in one minute
-    screen -S $screen_session -X stuff "say Warning! Server is shutting down in 1 minute!^M"
+    # Warn online players that it's shutting down in one minute
+    printf "Informing players that server is stopping.\n"
+    for i in {60..1}
+    do
+      printf "Server will stop in $i seconds...\r"
+      sleep 1
+    done &
+    screen -S $screen_session -X stuff "say Warning! Server is stopping in 1 minute!^M"
     sleep 10
     screen -S $screen_session -X stuff "/save-all^M"
     sleep 40
-    screen -S $screen_session -X stuff "say Shutting down in 10 seconds...^M"
+    screen -S $screen_session -X stuff "say Stopping in 10 seconds...^M"
     sleep 5
     for i in {5..1}
     do
-        screen -S $screen_session -X stuff "say Shutting down in $i...^M"
+        screen -S $screen_session -X stuff "say Stopping in $i...^M"
         sleep 1
     done
     sleep 1
